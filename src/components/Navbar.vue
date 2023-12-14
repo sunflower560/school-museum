6d41a3c700bb9483dd691b1e9039dcebfe79b57d
daaniill <116388121+sunflower560@users.noreply.github.com>
2023-12-14 18:41:14 +0300

    basic setting project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="navbar">
+    <router-link :to="MainPage">
+      Главная
+    </router-link>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {RouterLink} from 'vue-router'
+import MainPage from "@/pages/common/MainPage.vue";
+
+</script>
+
+<style scoped lang="scss">
+.navbar {
+
+}
+</style>
---
+++ src/pages/common/MainPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="main-page">
+    Hello Museum
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
