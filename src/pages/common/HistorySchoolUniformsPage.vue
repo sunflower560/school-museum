5ae2e00cfabb9bfe6ed953a79a46de0bbd00b5af
daaniill <116388121+sunflower560@users.noreply.github.com>
2024-01-23 00:37:59 +0300

    complete tasks methods filter and map

--- src/pages/common/HistorySchoolUniformsPage.vue
+++ src/pages/common/HistorySchoolUniformsPage.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="history-school-uniforms-page">
-<!--    <el-tag effect="dark" type="danger" size="large">MIN: {{ minItemValue }}</el-tag>-->
-<!--    <el-tag effect="dark" type="success" size="large">MAX: {{ maxItemValue }}</el-tag>-->
+    <el-tag effect="dark" type="danger" size="large">MIN: {{ minItemValue }}</el-tag>
+    <el-tag effect="dark" type="success" size="large">MAX: {{ maxItemValue }}</el-tag>
 
-<!--    <el-tag effect="dark" type="warning" size="large">SUM: {{ itemsSumma }}</el-tag>-->
-<!--    <el-tag effect="light" type="success" size="large">ODD: {{ oddItemCount }}</el-tag>-->
-<!--    <el-tag effect="light" type="danger" size="large">EVEN: {{ evenItemCount }}</el-tag>-->
+    <el-tag effect="dark" type="warning" size="large">SUM: {{ itemsSumma }}</el-tag>
+    <el-tag effect="light" type="success" size="large">ODD: {{ oddItemCount }}</el-tag>
+    <el-tag effect="light" type="danger" size="large">EVEN: {{ evenItemCount }}</el-tag>
 
     <el-button @click="addNewItem" type="primary">Add new number</el-button>
     <el-button @click="clearItems" type="danger">Clear Array</el-button>
 
     <el-row v-for="item in itemsSources" :key="item">
       <el-col>
-        <p>{{item.value}} <el-checkbox v-model="item.isActive" :label="item.isActive">{{item.isActive}}</el-checkbox></p>
+        <p>{{item.value}} <el-checkbox @click="items" v-model="item.isActive" :label="item.isActive">{{item.isActive}}</el-checkbox></p>
       </el-col>
     </el-row>
   </div>
@@ -33,43 +33,45 @@ const itemsSources = ref<Array<IItem>>([
   }
 ])
 
-const items = computed(() => {
-  const filteredItems = []
-  for(let i = 0; i < itemsSources.value.length; i++) {
-    if(itemsSources.value[i]?.isActive) {
-      filteredItems.push(itemsSources.value[i])
-    }
-  }
-  return filteredItems
-})
-
 const addNewItem = () => itemsSources.value.push({value: Math.floor(Math.random() * 100), isActive: true})
 const clearItems = () => itemsSources.value = []
-//
-// const maxItemValue = computed(() => !itemsSources.value.length ? 0 : Math.max(...itemsSources.value))
-// const minItemValue = computed(() => !itemsSources.value.length ? 0 : Math.min(...itemsSources.value))
-//
-// const itemsSumma = computed(() => itemsSources.value.reduce((previousValue, currentValue) => previousValue + currentValue, 0))
-//
-// const oddItemCount = computed(() => itemsSources.value.filter((number: number) => number % 2 !== 0).length)
-//
-// const evenItemCount = computed(() => itemsSources.value.length - oddItemCount.value)
+
+//filter + map
+const itemsFilterMap = computed(() => {
+  const filterItemValues = []
+  for(let i = 0; i < itemsSources.value.length; i++)
+    if(itemsSources.value[i]?.isActive && typeof itemsSources.value[i]?.value === 'number')
+      filterItemValues.push(itemsSources.value[i].value)
+  return filterItemValues
+})
+
+const items = computed(() => {
+  return itemsSources.value
+      .filter(item => item?.isActive && typeof item?.value === 'number')
+      .map(item => item.value)
+})
+
+
+const maxItemValue = computed(() => !items.value.length ? 0 : Math.max(...items.value))
+const minItemValue = computed(() => !items.value.length ? 0 : Math.min(...items.value))
+
+const itemsSumma = computed(() => items.value.reduce((previousValue, currentValue) => previousValue + currentValue, 0))
+
+const oddItemCount = computed(() => items.value.filter((number: number) => number % 2 !== 0).length)
+
+const evenItemCount = computed(() => items.value.length - oddItemCount.value)
 
 
 
-// const maxItemValue = computed(() => {
-//   let max = 0
-//   items.value.forEach((item, i, items) => items[i].value > max ? max = items[i].value : 0)
-//   return max
-// })
-//
-// const minItemValue = computed(() => {
-//   let min = 100
-//   if (!items.value.length) {
-//     min = 0
+// filter
+// const items = computed(() => {
+//   const filteredItems = []
+//   for(let i = 0; i < itemsSources.value.length; i++) {
+//     if(itemsSources.value[i]?.isActive) {
+//       filteredItems.push(itemsSources.value[i])
+//     }
 //   }
-//   items.value.forEach((item, i, items) => items[i].value < min ? min = items[i].value : 0)
-//   return min
+//   return filteredItems
 // })
 </script>
 
