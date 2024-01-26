16c6767224f729a0b8380b794a33adfe31c448c9
daaniill <116388121+sunflower560@users.noreply.github.com>
2024-01-26 13:33:26 +0300

    add array people and methods newHuman, summaSalary, filter gender

--- src/pages/common/HistorySchoolUniformsPage.vue
+++ src/pages/common/HistorySchoolUniformsPage.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="history-school-uniforms-page">
-    <el-tag effect="dark" type="danger" size="large">MIN: {{ minItemValue }}</el-tag>
-    <el-tag effect="dark" type="success" size="large">MAX: {{ maxItemValue }}</el-tag>
 
-    <el-tag effect="dark" type="warning" size="large">SUM: {{ itemsSumma }}</el-tag>
-    <el-tag effect="light" type="success" size="large">ODD: {{ oddItemCount }}</el-tag>
-    <el-tag effect="light" type="danger" size="large">EVEN: {{ evenItemCount }}</el-tag>
+    <el-row>
+      <el-col :lg="9">
+        <div class="history-school-uniforms-page-people">
+       <div class="history-school-uniforms-page-people-management">
+         <el-button @click="addNewHuman" type="success">
+           Add human
+         </el-button>
+         <br>
+         <el-tag effect="dark" type="warning" size="large">SUM: {{ peopleSalarySumma }}</el-tag>
+         <el-tag effect="light" type="success" size="large">MAN: {{ manHumanCount }}</el-tag>
+         <el-tag effect="light" type="danger" size="large">WOMAN: {{ womanHumanCount }}</el-tag>
+       </div>
+          <ul v-for="human in peopleSources" :key="human.id">
+            <li>
+              <el-button @click="removeHuman(human.id)" type="danger" size="small">
+                <p>X</p>
+              </el-button>
+              {{human.id}}) {{human.name}} {{human.lastName}} {{human.age}}, salary: {{human.salary}} ₽
+              <el-checkbox @click="items" v-model="human.isActive" :label="human.isActive">{{human.isActive}}</el-checkbox>
+              {{human.gender}}
+            </li>
+            <br>
+          </ul>
+        </div>
+      </el-col>
 
-    <el-button @click="addNewItem" type="primary">Add new number</el-button>
-    <el-button @click="clearItems" type="danger">Clear Array</el-button>
+      <el-col :lg="15">
+        <el-tag effect="dark" type="danger" size="large">MIN: {{ minItemValue }}</el-tag>
+        <el-tag effect="dark" type="success" size="large">MAX: {{ maxItemValue }}</el-tag>
 
-    <el-row v-for="item in itemsSources" :key="item">
-      <el-col>
-        <p>{{item.value}} <el-checkbox @click="items" v-model="item.isActive" :label="item.isActive">{{item.isActive}}</el-checkbox></p>
+        <el-tag effect="dark" type="warning" size="large">SUM: {{ itemsSumma }}</el-tag>
+        <el-tag effect="light" type="success" size="large">ODD: {{ oddItemCount }}</el-tag>
+        <el-tag effect="light" type="danger" size="large">EVEN: {{ evenItemCount }}</el-tag>
+
+        <el-button @click="addNewItem" type="primary">Add new number</el-button>
+        <el-button @click="clearItems" type="danger">Clear Array</el-button>
+
+        <el-row v-for="item in itemsSources" :key="item">
+          <el-col>
+            <p>{{item.value}} <el-checkbox @click="items" v-model="item.isActive" :label="item.isActive">{{item.isActive}}</el-checkbox></p>
+          </el-col>
+        </el-row>
       </el-col>
     </el-row>
   </div>
@@ -21,10 +51,60 @@
 <script setup lang="ts">
 import {computed, ref} from "vue";
 
+interface IHuman {
+  id: number,
+  name: string,
+  lastName: string,
+  age: number,
+  salary: number,
+  isActive: boolean,
+  gender: string
+}
+
 interface IItem{
   value:number
   isActive:boolean
 }
+const namesMan = ['Max', 'Vlad', 'Andrei']
+const namesWoman = ['Sofia', 'Elena', 'Maria']
+const lastNames = ['Sidorenko', 'Aksenchik', 'Trifonova', 'Sinitsen']
+const ages = [15, 25, 22, 42, 35, 38, 32, 56, 20, 19, 18, 24]
+const salary = [30000, 25000, 27000, 80000, 15000, 200000, 400000, 45000, 37000]
+
+const peopleSources = ref<Array<IHuman>>([
+  {id: 1, name: 'Max', lastName: 'Sidorenko', age: 27, salary: 30000, isActive: true, gender: 'Man'},
+  {id: 2, name: 'Vlad', lastName: 'Aksenchik', age: 24, salary: 25000, isActive: true, gender: 'Man'},
+  {id: 3, name: 'Sofia', lastName: 'Trifonova', age: 31, salary: 27000, isActive: true, gender: 'Woman'},
+  {id: 4, name: 'Andrei', lastName: 'Sinitsen', age: 24, salary: 80000, isActive: true, gender: 'Man'}
+])
+
+const people = computed(() => peopleSources.value
+    .filter(human => human?.isActive && typeof human?.salary === 'number')
+    .map(human => ({salary: human.salary, gender: human.gender})))
+
+const addNewHuman = () => {
+  const humanId = peopleSources.value.length + 1
+  const name = [namesWoman, namesMan][Math.floor(Math.random() * 2)][Math.floor(Math.random() * 3)]
+  const gender = namesMan.includes(name) ? 'Man' : 'Woman'
+  peopleSources.value.push({
+    id: humanId,
+    name: name,
+    lastName: lastNames[Math.floor(Math.random() * 4)],
+    age: ages[Math.floor(Math.random() * 12)],
+    salary: salary[Math.floor(Math.random() * 9)],
+    isActive: true,
+    gender: gender
+  })
+}
+const peopleSalarySumma = computed(() => people.value.reduce((previousSalary, currentSalary) => previousSalary + currentSalary.salary, 0))
+const removeHuman = (id: number) => peopleSources.value = peopleSources.value.filter(human => human.id !== id)
+
+const manHumanCount = computed(() => people.value.filter(human => human.gender === 'Man').length)
+const womanHumanCount = computed(() => people.value.length - manHumanCount.value)
+
+
+
+
 
 const itemsSources = ref<Array<IItem>>([
   {
@@ -92,6 +172,19 @@ const evenItemCount = computed(() => items.value.length - oddItemCount.value)
 
   .el-tag {
     margin-right: $radio_average;
+  }
+
+  ul {
+    list-style-type: none;
+  }
+  &-people {
+    &-management {
+      margin-bottom: $radio_average;
+      .el-tag {
+        margin-right: $radio_average;
+        margin-top: $radius_medium;
+      }
+    }
   }
 }
 </style>
